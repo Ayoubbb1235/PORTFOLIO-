33e9a850b9b3b4850202616bc9cd8dd4f6780196
Ayoubbb1235 <159105060+Ayoubbb1235@users.noreply.github.com>
2024-12-13 23:59:42 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,169 @@
+// htmlcss progress circular bar 
+let htmlProgress = document.querySelector(".html-css"),
+  htmlValue = document.querySelector(".html-progress");
+
+let htmlStartValue = 0,
+  htmlEndValue = 90,
+  htmlspeed = 30;
+
+let progresshtml = setInterval(() => {
+  htmlStartValue++;
+
+  htmlValue.textContent = `${htmlStartValue}%`;
+  htmlProgress.style.background = `conic-gradient(#E34F26 ${
+    htmlStartValue * 3.6
+  }deg, #ededed 0deg)`;
+
+  if (htmlStartValue == htmlEndValue) {
+    clearInterval(progresshtml);
+  }
+}, htmlspeed);
+
+// javasript progress circular bar 
+let javascriptProgress = document.querySelector(".javascript"),
+  javascriptValue = document.querySelector(".javascript-progress");
+
+let javascriptStartValue = 0,
+  javascriptEndValue = 75,
+  jsspeed = 30;
+
+let progressjs = setInterval(() => {
+  javascriptStartValue++;
+
+  javascriptValue.textContent = `${javascriptStartValue}%`;
+  javascriptProgress.style.background = `conic-gradient(#F7DF1E ${
+    javascriptStartValue * 3.6
+  }deg, #ededed 0deg)`;
+
+  if (javascriptStartValue == javascriptEndValue) {
+    clearInterval(progressjs);
+  }
+}, jsspeed);
+
+
+// reactjs progress circular bar 
+let reactProgress = document.querySelector(".reactjs"),
+  reactValue = document.querySelector(".reactjs-progress");
+
+let reactStartValue = 0,
+  reactEndValue = 40,
+  rjsspeed = 30;
+
+let progressreact = setInterval(() => {
+  reactStartValue++;
+
+  reactValue.textContent = `${reactStartValue}%`;
+  reactProgress.style.background = `conic-gradient(#61DAFB ${
+    reactStartValue * 3.6
+  }deg, #ededed 0deg)`;
+
+  if (reactStartValue == reactEndValue) {
+    clearInterval(progressreact);
+  }
+}, rjsspeed);
+
+
+// typescript progress circular bar 
+let typeProgress = document.querySelector(".TypeScript"),
+  typeValue = document.querySelector(".typejs-progress");
+
+let typeStartValue = 0,
+  typeEndValue = 50,
+  tjsspeed = 30;
+
+let progresstype = setInterval(() => {
+  typeStartValue++;
+
+  typeValue.textContent = `${typeStartValue}%`;
+  typeProgress.style.background = `conic-gradient(#3178C6 ${
+    typeStartValue * 3.6
+  }deg, #ededed 0deg)`;
+
+  if (typeStartValue == typeEndValue) {
+    clearInterval(progresstype);
+  }
+}, tjsspeed);
+
+
+
+// typescript progress circular bar 
+let tailProgress = document.querySelector(".Tailwind"),
+  tailValue = document.querySelector(".Tailwind-progress");
+
+let tailStartValue = 0,
+  tailEndValue = 65,
+  tlspeed = 30;
+
+let progresstail = setInterval(() => {
+  tailStartValue++;
+
+  tailValue.textContent = `${tailStartValue}%`;
+  tailProgress.style.background = `conic-gradient(#38BDF8 ${
+    tailStartValue * 3.6
+  }deg, #ededed 0deg)`;
+
+  if (tailStartValue == tailEndValue) {
+    clearInterval(progresstail);
+  }
+}, tlspeed);
+
+
+// filter using javascript
+$(document).ready(function () {
+  $(".filter-item").click(function () {
+    const value = $(this).attr("data-filter");
+    if (value == "all") {
+      $(".post").show("1000");
+    } else {
+      $(".post")
+        .not("." + value)
+        .hide("1000");
+      $(".post")
+        .filter("." + value)
+        .show("1000");
+    }
+  });
+});
+
+
+// javascript for sticky navbar even if u scroll the navbar will be fixed
+document.addEventListener("DOMContentLoaded", function(){
+  window.addEventListener('scroll', function() {
+      if (window.scrollY > 50) {
+        document.getElementById('navbar-top').classList.add('fixed-top');
+        // add padding top to show content behind navbar
+        navbar_height = document.querySelector('.navbar').offsetHeight;
+        document.body.style.paddingTop = navbar_height + 'px';
+      } else {
+        document.getElementById('navbar-top').classList.remove('fixed-top');
+         // remove padding top from body
+        document.body.style.paddingTop = '0';
+      } 
+  });
+}); 
+
+
+// adding funtionality to back to top button 
+
+//Get the button
+let mybutton = document.getElementById("btn-back-to-top");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function () {
+  scrollFunction();
+};
+function scrollFunction() {
+  if (
+    document.body.scrollTop > 20 ||
+    document.documentElement.scrollTop > 20
+  ) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+// When the user clicks on the button, scroll to the top of the document
+mybutton.addEventListener("click",function(){
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+});
